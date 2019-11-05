a501bd9c07536cda4b93ddd9ee074ad970f9c84f
Peter Prokop <peter.prokop@blackswan.com>
2019-11-05 16:57:11 +0000

    Initial Commit

---
+++ Bogracz/Bogracz.h
@@ -0,0 +1,19 @@
+//
+//  Bogracz.h
+//  Bogracz
+//
+//  Created by Pete Prokop on 05/11/2019.
+//  Copyright © 2019 Pete Prokop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bogracz.
+FOUNDATION_EXPORT double BograczVersionNumber;
+
+//! Project version string for Bogracz.
+FOUNDATION_EXPORT const unsigned char BograczVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bogracz/PublicHeader.h>
+
+
